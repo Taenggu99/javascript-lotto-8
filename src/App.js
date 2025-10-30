94a81a8286aa339610830d971669c38671eab0c5
Taenggu99 <leetaeeun99@gmail.com>
2025-10-30 15:14:57 +0900

    <feat>: #3 구매 장수 계산

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ class App {
     const purchaseAmount = await this.getpurchaseAmount();
 
     // #2 구입 금액 유효성 검사
-if (!this.isValidAmount(purchaseAmount)){
-  throw new Error ("[Error] 구입 금액은 1000원 단위로 입력해주세요")
-}
+    if (!this.isValidAmount(purchaseAmount)) {
+      throw new Error("[Error] 구입 금액은 1000원 단위로 입력해주세요");
+    }
     // #3 구매 장수 계산
+    const ticketCount = purchaseAmount / 1000;
+    console.log(`${ticketCount}개를 구매했습니다.`);
+
     // #5 로또 번호 출력
     // #6 당첨 번호 입력
     // #8 보너스 번호 입력
@@ -23,12 +26,12 @@ if (!this.isValidAmount(purchaseAmount)){
     return Number(input.trim());
   }
   // #2 구입 금액 유효성 검사 함수
-isValidAmount(amount){
-  if(isNaN(amount) || amount<=0 || amount%1000 !==0){
-    return false;
+  isValidAmount(amount) {
+    if (isNaN(amount) || amount <= 0 || amount % 1000 !== 0) {
+      return false;
+    }
+    return true;
   }
-  return true;
-}
   // #3 구매 장수 계산 함수
   // #5 로또 번호 출력 함수
   // #6 당첨 번호 입력 함수
