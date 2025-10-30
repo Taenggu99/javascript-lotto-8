71a3249846f5ce951f7ee826104497f69763960f
Taenggu99 <leetaeeun99@gmail.com>
2025-10-30 14:35:33 +0900

    <faet>: #1 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-class App {
-  async run() {}
+import { Random, Console } from "@woowacourse/mission-utils";
 
-// #1 구입 금액 입력        
-// #2 구입 금액 유효성 검사 
-// #3 구매 장수 계산        
-// #5 로또 번호 출력        
-// #6 당첨 번호 입력        
-// #8 보너스 번호 입력      
-// #12 당첨 통계 출력        
-// #13 수익률 계산 및 출력   
+class App {
+  async run() {
+    // #1 구입 금액 입력
+    const purchaseAmount = await this.getpurchaseAmount();
+
+    // #2 구입 금액 유효성 검사
+    // #3 구매 장수 계산
+    // #5 로또 번호 출력
+    // #6 당첨 번호 입력
+    // #8 보너스 번호 입력
+    // #12 당첨 통계 출력
+    // #13 수익률 계산 및 출력
+  }
+  // #1 구입 금액 입력 함수
+  async getpurchaseAmount() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요");
+    return Number(input.trim());
+  }
 }
 
 export default App;
