c34d00ef32b4f3d17722cf5821834f3620147f2e
Taenggu99 <leetaeeun99@gmail.com>
2025-10-30 15:05:42 +0900

    <feat>: #2 구입 금액 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
   async run() {
@@ -6,6 +7,9 @@ class App {
     const purchaseAmount = await this.getpurchaseAmount();
 
     // #2 구입 금액 유효성 검사
+if (!this.isValidAmount(purchaseAmount)){
+  throw new Error ("[Error] 구입 금액은 1000원 단위로 입력해주세요")
+}
     // #3 구매 장수 계산
     // #5 로또 번호 출력
     // #6 당첨 번호 입력
@@ -18,6 +22,19 @@ class App {
     const input = await Console.readLineAsync("구입금액을 입력해 주세요");
     return Number(input.trim());
   }
+  // #2 구입 금액 유효성 검사 함수
+isValidAmount(amount){
+  if(isNaN(amount) || amount<=0 || amount%1000 !==0){
+    return false;
+  }
+  return true;
+}
+  // #3 구매 장수 계산 함수
+  // #5 로또 번호 출력 함수
+  // #6 당첨 번호 입력 함수
+  // #8 보너스 번호 입력 함수
+  // #12 당첨 통계 출력 함수
+  // #13 수익률 계산 및 출력 함수
 }
 
 export default App;
